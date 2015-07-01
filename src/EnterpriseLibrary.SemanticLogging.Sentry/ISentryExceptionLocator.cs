167029bea029992587d73faee85e2dcc7bf17a93
Richard Szalay <richard.szalay@loweprofero.com>
2015-07-01 11:55:02 +1000

    Added handling for failed formatted exception parsing

--- src/EnterpriseLibrary.SemanticLogging.Sentry/ISentryExceptionLocator.cs
+++ src/EnterpriseLibrary.SemanticLogging.Sentry/ISentryExceptionLocator.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Diagnostics.Contracts;
 using SharpRaven.Data;
 
 namespace EnterpriseLibrary.SemanticLogging.Sentry
@@ -7,54 +6,5 @@ namespace EnterpriseLibrary.SemanticLogging.Sentry
     public interface ISentryExceptionLocator
     {
         ICollection<SentryException> Locate(Dictionary<string, object> payload);
-    }
-
-    public class FormattedExceptionLocator : ISentryExceptionLocator
-    {
-        private readonly string _payloadKey;
-
-        /// <summary>
-        /// Initializes a new FormattedExceptionLocator using the "exception" payload key
-        /// </summary>
-        public FormattedExceptionLocator()
-            : this("exception")
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new FormattedExceptionLocator using the specified payload key
-        /// </summary>
-        /// <param name="payloadKey">The key used to extract formatted exception data from an event's payload</param>
-        public FormattedExceptionLocator(string payloadKey)
-        {
-            Contract.Requires(payloadKey != null);
-
-            _payloadKey = payloadKey;
-        }
-
-        public ICollection<SentryException> Locate(Dictionary<string, object> payload)
-        {
-            string formattedExceptionPayload = LocateExceptionPayload(payload, _payloadKey);
-
-            if (formattedExceptionPayload == null)
-                return new SentryException[0];
-
-            var parsedExceptions = SentryExceptionParser.Parse(formattedExceptionPayload);
-
-            if (parsedExceptions.Count > 0)
-                payload.Remove(_payloadKey);
-
-            return parsedExceptions;
-        }
-
-        static string LocateExceptionPayload(IDictionary<string, object> payload, string key)
-        {
-            if (!payload.ContainsKey(key))
-                return null;
-
-            object exceptionPayload = payload[key];
-
-            return exceptionPayload as string;
-        }
     }
 }
